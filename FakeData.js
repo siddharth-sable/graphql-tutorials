ff192227f4d439c20b0c17c46dd802aa99fa9747
Siddharth Sabale <siddharth.sable4618@gmail.com>
2022-11-15 12:25:51 +0530

    initialCommit

---
+++ FakeData.js
@@ -0,0 +1,39 @@
+const UserList = [
+  {
+    id: 1,
+    name: "John",
+    username: "john",
+    age: 20,
+    nationality: "Canada",
+  },
+  {
+    id: 2,
+    name: "Johnathan",
+    username: "johnathan",
+    age: 21,
+    nationality: "Canada",
+  },
+  {
+    id: 3,
+    name: "Siddharth",
+    username: "siddo",
+    age: 20,
+    nationality: "India",
+  },
+  {
+    id: 4,
+    name: "Jiyan",
+    username: "jiyan",
+    age: 20,
+    nationality: "India",
+  },
+  {
+    id: 5,
+    name: "Ritish",
+    username: "ritish",
+    age: 20,
+    nationality: "India",
+  },
+];
+
+module.exports = { UserList };
